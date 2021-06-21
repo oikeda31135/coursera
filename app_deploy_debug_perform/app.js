ca30dc5871be49e016cfcf000db9053fc0fac810
ikeda-osamu <io.01265@konami.com>
2021-06-21 22:23:26 +0900

    追加

---
+++ app_deploy_debug_perform/app.js
@@ -0,0 +1,12 @@
+const http = require('http')
+
+const server = http.createServer((req, res) => {
+        console.log(`Request received at: ${new Date()}`);
+        res.end('Hello containers...');
+    });
+
+console.log('Server starting...');
+
+server.listen(8080, () => {
+        console.log('Started.')
+    });
